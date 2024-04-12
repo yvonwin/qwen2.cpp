6053fc3144f7ea6bf04b14ec0fea929f41b1c4a1
yvonwin <wangkuixz@gmail.com>
2024-04-12 09:42:34 +0800

    Remove unnecessary comment in tiktoken.h

--- tiktoken.h
+++ tiktoken.h
@@ -178,7 +178,7 @@ class tiktoken {
 				}
 
 				if (allowed_special.count(special) == 1) {				
-					return { std::move(special), re2::StringPiece(&*start, input.begin() - start - special.size()) }; // failed on windows building
+					return { std::move(special), re2::StringPiece(&*start, input.begin() - start - special.size()) };
 				}
 			}
 			return { std::nullopt, input };
